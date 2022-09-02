0c2bca4ce02751e92bf4070d194c8b21e0668dba
rehan-c <rehanchoudhery@gmail.com>
2022-09-02 17:40:48 +0100

    Updates

---
+++ precache-manifest.0a8fdca9cdec0b15482dc629e0cc7928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5904d9bc4d0eaef9c9d0fbcdad635eb3",
+    "url": "/react-firebase-gallery/index.html"
+  },
+  {
+    "revision": "f4411d9abce3f91f93f8",
+    "url": "/react-firebase-gallery/static/css/main.28585e81.chunk.css"
+  },
+  {
+    "revision": "987a5f22b4805d77f35b",
+    "url": "/react-firebase-gallery/static/js/2.1098431b.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/react-firebase-gallery/static/js/2.1098431b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4411d9abce3f91f93f8",
+    "url": "/react-firebase-gallery/static/js/main.4c9604b9.chunk.js"
+  },
+  {
+    "revision": "6e4b6f1b02b6f1493198",
+    "url": "/react-firebase-gallery/static/js/runtime-main.a1b93de3.js"
+  }
+]);
